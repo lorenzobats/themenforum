02589421dc8be2ebc14e61457014a3618b7e50ae
lbattist <lorenzo.battiston@hs-osnabrueck.de>
2023-01-26 13:21:24 +0100

    UI Components fuer Topic und Post Overview erstellt

--- src/main/java/de/hsos/swa/infrastructure/ui/PublicEndpoint.java
+++ src/main/java/de/hsos/swa/infrastructure/ui/PublicEndpoint.java
@@ -1,10 +1,8 @@
 package de.hsos.swa.infrastructure.ui;
 
+import de.hsos.swa.application.input.*;
+import de.hsos.swa.application.input.dto.in.GetPostByIdInputPortRequest;
 import de.hsos.swa.application.use_case_query.PostFilterParams;
-import de.hsos.swa.application.input.GetAllPostsInputPort;
-import de.hsos.swa.application.input.GetAllTopicsInputPort;
-import de.hsos.swa.application.input.GetFilteredPostsInputPort;
-import de.hsos.swa.application.input.GetTopicByIdInputPort;
 import de.hsos.swa.application.input.dto.in.GetFilteredPostInputPortRequest;
 import de.hsos.swa.application.input.dto.in.GetTopicByIdInputPortRequest;
 import de.hsos.swa.application.util.Result;
@@ -17,15 +15,13 @@ import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//TODO Error Templates erstellen
 
 @Path("/ui/public")
 @PermitAll
@@ -39,6 +35,9 @@ public class PublicEndpoint {
     GetFilteredPostsInputPort getFilteredPostsInputPort;
 
     @Inject
+    GetPostByIdInputPort getPostByIdInputPort;
+
+    @Inject
     GetAllTopicsInputPort getAllTopicsInputPort;
 
     @Inject
@@ -46,7 +45,8 @@ public class PublicEndpoint {
 
     @CheckedTemplate
     public static class Templates {
-        public static native TemplateInstance home(List<Post> allPosts);
+        public static native TemplateInstance posts(List<Post> allPosts);
+        public static native TemplateInstance post(Post post);
         public static native TemplateInstance topics(List<Topic> allTopics);
         public static native TemplateInstance topic(Topic topic, List<Post> posts);
     }
@@ -54,15 +54,27 @@ public class PublicEndpoint {
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/posts")
-    public TemplateInstance home(@QueryParam("topic") String topic) {
+    public TemplateInstance posts(@QueryParam("topic") String topic) {
         if(topic != null) {
             Map<PostFilterParams, Object> filterParams = new HashMap<>();
             filterParams.put(PostFilterParams.TOPIC, topic);
             Result<List<Post>> filteredPosts = getFilteredPostsInputPort.getFilteredPosts(new GetFilteredPostInputPortRequest(filterParams, false));
-            return Templates.home(filteredPosts.getData());
+            return Templates.posts(filteredPosts.getData());
         }
         Result<List<Post>> allPosts = getAllPostsInputPort.getAllPosts(false);
-        return Templates.home(allPosts.getData());
+        return Templates.posts(allPosts.getData());
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    @Path("/posts/{id}")
+    public TemplateInstance post(@PathParam("id") String id,@DefaultValue("true") @QueryParam("includeComments") boolean includeComments) {
+        Result<Post> postResult = getPostByIdInputPort.getPostById(new GetPostByIdInputPortRequest(id, includeComments));
+        if(postResult.isSuccessful()) {
+            return Templates.post(postResult.getData());
+        }
+
+        return Templates.post(null);
     }
 
     @GET
@@ -76,7 +88,7 @@ public class PublicEndpoint {
     @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/topics/{id}")
-    public TemplateInstance topics(@PathParam("id") String id) {
+    public TemplateInstance topic(@PathParam("id") String id) {
         Result<Topic> topicResult = getTopicByIdInputPort.getTopicById(new GetTopicByIdInputPortRequest(id));
         if(topicResult.isSuccessful()) {
             Map<PostFilterParams, Object> filterParams = new HashMap<>();
