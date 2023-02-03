769566bad43edc9c824d133a87cb9fe744b2bd19
lbattist <lorenzo.battiston@hs-osnabrueck.de>
2023-02-03 11:48:00 +0100

    Validation Annotiations bei RequestBodies

--- src/main/java/de/hsos/swa/actors/rest/dto/in/CommentPostRequestBody.java
+++ src/main/java/de/hsos/swa/actors/rest/dto/in/CommentPostRequestBody.java
@@ -4,13 +4,14 @@ import de.hsos.swa.actors.rest.dto.in.validation.ValidatedRequestBody;
 import de.hsos.swa.application.input.dto.in.CommentPostCommand;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 @Schema(name = "CommentCreationDto", required = true)
 public class CommentPostRequestBody implements ValidatedRequestBody {
-    @NotEmpty
+    @NotBlank(message = "postId is blank")
     public String postId;
-    @NotEmpty
+
+    @NotBlank(message = "text is blank")
     public String text;
 
     public static class Converter {
